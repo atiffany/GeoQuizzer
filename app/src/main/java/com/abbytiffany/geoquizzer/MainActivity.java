681837f762e7706c32ba4f4bc6b301fcaa69a3d1
atiffany <abby.tiffany@gmail.com>
2018-04-09 22:31:31 -0700

    randomization of answer choices

--- app/src/main/java/com/abbytiffany/geoquizzer/MainActivity.java
+++ app/src/main/java/com/abbytiffany/geoquizzer/MainActivity.java
@@ -3,6 +3,7 @@ package com.abbytiffany.geoquizzer;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -29,14 +30,19 @@ public class MainActivity extends AppCompatActivity {
         return answers;
     }
 
-    public int getRandomNumber(String[] arr) {
-        int newRandom = new Random().nextInt(arr.length);
+    public int getRandomNumber(int length) {
+        int newRandom = new Random().nextInt(length);
         return newRandom;
     }
-    public int getWrongAnswer(int correct){
-        int newRand = getRandomNumber(getAnswers());
+    public int getRightAnswer() {
+        int newRand = getRandomNumber(2);
+        Log.v( "random num", Integer.toString(newRand));
+        return newRand;
+    }
+    public int getWrongAnswer(int correct) {
+        int newRand = getRandomNumber(getAnswers().length);
         while (newRand == correct) {
-            newRand = getRandomNumber(getAnswers());
+            newRand = getRandomNumber(getAnswers().length);
         }
         return newRand;
     }
@@ -51,20 +57,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void populateFirstQuestionAndAnswer(View view) {
-        global = getRandomNumber(getQuestions());
+        global = getRandomNumber(getQuestions().length);
+        int correctAnswerNumber = getRightAnswer();
+        if (correctAnswerNumber == 1) {
+            populate(R.id.answer_text_1, getAnswers(), global);
+            populate(R.id.answer_text_2, getAnswers(), getWrongAnswer(global));
+        } else {
+            populate(R.id.answer_text_2, getAnswers(), global);
+            populate(R.id.answer_text_1, getAnswers(), getWrongAnswer(global));
+        }
 
         populate(R.id.question_text, getQuestions(), global);
-        populate(R.id.answer_correct_text, getAnswers(), global);
-        populate(R.id.answer_incorrect_text, getAnswers(), getWrongAnswer(global));
         displayScore(R.id.current_score_number, userScore);
         displayScore(R.id.highest_possible_score_number, attempts);
     }
     public void populateNewQuestionAndAnswer() {
-        int global = getRandomNumber(getQuestions());
+        global = getRandomNumber(getQuestions().length);
+        int correctAnswerNumber = getRightAnswer();
+        if (correctAnswerNumber == 1) {
+            populate(R.id.answer_text_1, getAnswers(), global);
+            populate(R.id.answer_text_2, getAnswers(), getWrongAnswer(global));
+        } else {
+            populate(R.id.answer_text_2, getAnswers(), global);
+            populate(R.id.answer_text_1, getAnswers(), getWrongAnswer(global));
+        }
 
         populate(R.id.question_text, getQuestions(), global);
-        populate(R.id.answer_correct_text, getAnswers(), global);
-        populate(R.id.answer_incorrect_text, getAnswers(),getWrongAnswer(global));
         displayScore(R.id.current_score_number, userScore);
         displayScore(R.id.highest_possible_score_number, attempts);
     }
@@ -90,5 +108,21 @@ public class MainActivity extends AppCompatActivity {
                 populateNewQuestionAndAnswer();
             }
         }, 500);
+    }
+    public void checkIfCorrect(View view) {
+        String input = ((TextView)view).getText().toString();
+        String answer = getAnswers()[global];
+        Log.v("Answers: ", input);
+        Log.v("Actual: ", answer);
+        if (((TextView)view).getText().toString().equals(getAnswers()[global])) {
+            answerWasCorrect(view);
+        } else {
+            answerWasIncorrect(view);
+        }
+    }
+    public void resetScore(View view){
+        userScore = 0;
+        attempts = 0;
+        populateNewQuestionAndAnswer();
     }
 }
